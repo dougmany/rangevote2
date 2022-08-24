07ce7902faceb6e8d26f2e6fd36b58274767928f
Doug Meeker <wog400@hotmail.com>
2022-08-23 22:31:14 -0700

    Undo random sort.

--- RangeVote2/Data/RangeVoteRepository.cs
+++ RangeVote2/Data/RangeVoteRepository.cs
@@ -216,7 +216,7 @@ namespace RangeVote2.Data
                 return ballot;
             }
 
-            return new Ballot { Id = guid, Candidates = DefaultCandidates.OrderBy(a => Guid.NewGuid()).ToArray()  };
+            return new Ballot { Id = guid, Candidates = DefaultCandidates  };
         }
 
         public async Task PutBallotAsync(Ballot ballot)
